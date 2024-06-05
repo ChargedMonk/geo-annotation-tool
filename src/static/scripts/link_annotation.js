8b6953546932a8ca745012af270dc7f967cfde5c
Vatsalya Bajpai <vatsalya.bajpai@klearnow.com>
2024-06-05 15:05:47 +0530

    Added header checkbox

--- src/static/scripts/link_annotation.js
+++ src/static/scripts/link_annotation.js
@@ -14,6 +14,35 @@ const handleDeleteKeyValue = (e) => {
 };
 
 const handleAddKeyValue = (e) => {
+    const uuid = crypto.randomUUID();
+    const key_value_row = `<div class="row m-1 gy-1 gx-1 key_value d-flex justify-content-evenly">
+                            <div class="col-1 p-0">
+                                <button type="button" class="btn delete_key_value_btn"><i class="bi bi-trash"></i></button>
+                            </div>
+                            <div class="col-1 flex-grow-1">
+                                <input class="form-control standard-key-dropdown invalid-input" list="standardKeyOptionsList"
+                                    placeholder="Standard Key" aria-label="Standard Key">
+                            </div>
+                            <div class="col-1 d-flex justify-content-center flex-grow-1">
+                                <input type="checkbox" class="btn-check" id="btn-check-outlined_${uuid}" autocomplete="off">
+                                <label class="btn btn-outline-primary" for="btn-check-outlined_${uuid}">Header</label><br>
+                            </div>
+                            <div class="col-1 flex-grow-1">
+                                <input type="text" class="form-control" placeholder="LI No."
+                                aria-label="Belongs to which line item row">
+                            </div>
+                            <div class="col-1 flex-grow-1">
+                                <input type="text" class="form-control field_to_be_annotated key"
+                                    placeholder="Key" aria-label="Key">
+                            </div>
+                            <div class="col-2 flex-grow-1">
+                                <input type="text" class="form-control  field_to_be_annotated value" placeholder="Value"
+                                    aria-label="Value">
+                            </div>
+                            <div class="col-1 p-0">
+                                <button type="button" class="btn add_key_value_btn"><i class="bi bi-plus-lg"></i></button>
+                            </div>
+                        </div>`;
     let currentKeyValueRow = null;
     if (e.target.tagName === "BUTTON") {
         currentKeyValueRow = e.target.parentElement.parentElement;
@@ -28,43 +57,18 @@ const handleAddKeyValue = (e) => {
     currentKeyValueRow.nextElementSibling.lastElementChild.firstElementChild.addEventListener('click', handleAddKeyValue);
     // Add focus listener for fields to be annotated
     const siblingChildren = currentKeyValueRow.nextElementSibling.children;
-    for (let idx = 3; idx < siblingChildren.length - 1; idx++) {
+    for (let idx = 4; idx < siblingChildren.length - 1; idx++) {
         siblingChildren[idx].firstElementChild.addEventListener('focus', handleSelectField);
     }
     // Add change listener for standard key dropdown
     siblingChildren[1].firstElementChild.addEventListener('change', handleChangeInStandardKey);
-    currentKeyValueRow.nextElementSibling.children[3].firstElementChild.dispatchEvent(new Event('focus'));
+    currentKeyValueRow.nextElementSibling.children[4].firstElementChild.dispatchEvent(new Event('focus'));
 };
 
 
 
 let delete_key_value_btns = document.getElementsByClassName("delete_key_value_btn");
 let add_key_value_btns = document.getElementsByClassName("add_key_value_btn");
-
-const key_value_row = `<div class="row m-1 gy-1 gx-1 key_value">
-                            <div class="col-1 p-0">
-                                <button type="button" class="btn delete_key_value_btn"><i class="bi bi-trash"></i></button>
-                            </div>
-                            <div class="col-2 flex-grow-1">
-                                <input class="form-control standard-key-dropdown invalid-input" list="standardKeyOptionsList"
-                                    placeholder="Standard Key" aria-label="Standard Key">
-                            </div>
-                            <div class="col-2 flex-grow-1">
-                                <input type="text" class="form-control" placeholder="LI No."
-                                aria-label="Belongs to which line item row">
-                            </div>
-                            <div class="col-3 flex-grow-1">
-                                <input type="text" class="form-control field_to_be_annotated key"
-                                    placeholder="Key" aria-label="Key">
-                            </div>
-                            <div class="col-3 flex-grow-1">
-                                <input type="text" class="form-control  field_to_be_annotated value" placeholder="Value"
-                                    aria-label="Value">
-                            </div>
-                            <div class="col-1 p-0">
-                                <button type="button" class="btn add_key_value_btn"><i class="bi bi-plus-lg"></i></button>
-                            </div>
-                        </div>`;
 
 
 
